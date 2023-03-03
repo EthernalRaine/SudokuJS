86b00ed1e7742323a3755f64b6b8a0c3e894d80b
Lu <113054610+EthernalRaine@users.noreply.github.com>
2023-03-03 09:57:04 +0100

    Delete Sudoku/src directory

--- Sudoku/src/sudoku.cs
+++
@@ -1,6 +0,0 @@
-public class Sudoku {
-    public string? Board { get; set; }
-    public int timestamp { get; set; }
-    public int backtracks { get; set; }
-    public int elapsed { get; set; }
-}
